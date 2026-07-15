33dd40cb388fa4387643e894e1aa2bec58c2047b
weiching07 <zxcvb282@gmail.com>
2026-07-15 17:40:50 +0800

    Add new brand dashboard config

---
+++ public/new-brand/dashboard-config.js
@@ -0,0 +1,44 @@
+window.DASHBOARD_CONFIG = {
+  defaultTitle: 'NEW BRAND 評論分析儀表板',
+
+  stores: [
+    {
+      brand: 'LILLA',
+      brandLabel: 'LILLA',
+      stores: [
+        {
+          store: 'DREAM PLAZA',
+          label: 'DREAM PLAZA 店',
+          title: 'LillA DREAM PLAZA店 評論分析儀表板'
+        }
+      ]
+    },
+    {
+      brand: 'SALT&STONE',
+      brandLabel: 'SALT&STONE',
+      aliases: ['SALT & STONE', 'SALTSTONE', 'SALT_STONE'],
+      stores: [
+        {
+          store: '南港',
+          label: '南港 店'
+        },
+        {
+          store: '101',
+          label: '101 店'
+        }
+      ]
+    },
+    {
+      brand: '泰勒肉舖',
+      brandLabel: '泰勒肉舖',
+      aliases: ['Taylor Butchery', 'TAYLOR BUTCHERY', '泰勒'],
+      stores: [
+        {
+          store: '台北',
+          label: '台北 店',
+          title: '泰勒肉舖 台北店 評論分析儀表板'
+        }
+      ]
+    }
+  ]
+};
