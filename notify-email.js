635c953f1984ef5d8e0db5e50825aa2517aa9b1b
weiching07 <zxcvb282@gmail.com>
2026-07-16 09:39:46 +0800

    Fix email sender address

--- notify-email.js
+++ notify-email.js
@@ -154,7 +154,7 @@ async function sendNewReviewEmail(newReviews) {
   `;
 
   await transporter.sendMail({
-    from: `"Google 評論通知" <tgifit.tgif@msa.hinet.net>`,
+    from: `"Google 評論通知" <${user}>`,
     to,
     subject,
     html
