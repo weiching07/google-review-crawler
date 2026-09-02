a4c400a1c2d27757675eec783483c28d9d268c80
weiching07 <zxcvb282@gmail.com>
2026-09-02 14:28:05 +0800

    Update store config

--- stores.js
+++ stores.js
@@ -13,7 +13,7 @@ module.exports = [
     store: 'DREAM PLAZA',
     storeCode: '9002',
     branch: 'LILLA',
-    keyword: 'LillA 台北',
+    keyword: 'LillA 評論',
     fallbackUrl: googleMapsSearch('LillA DREAM PLAZA')
   },
   {
